f2c822d1ead973fe126f490d3171b9b2ed5be3ff
Romário Lima <me@romar.io>
2016-02-16 01:32:18 -0200

    refactor(directives/ng-bind-custom): possibilita adição de atributo customizado

---
+++ app/js/directives/ng-bind-custom.js
@@ -0,0 +1,24 @@
+import angular from 'angular';
+
+export default {
+  name: 'ngBindCustom',
+  fn: ngBindCustom
+};
+
+function ngBindCustom($compile) {
+  'ngInject';
+
+  return {
+    restrict: 'AC',
+    compile: function ngBindCustomCompile(templateElement) {
+      $compile.$$addBindingClass(templateElement);
+      return function ngBindContentLink(scope, element, attr) {
+        $compile.$$addBindingInfo(element, attr.ngBindCustom);
+        element = element[0];
+        scope.$watch(attr.ngBindCustom, function ngBindCustonWatchAction(value) {
+          element.setAttribute(attr.customAttr, angular.isUndefined(value) ? '' : value);
+        });
+      };
+    }
+  };
+}
