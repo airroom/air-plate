10353377e1973aaca10db4d1139f4b1a20a333c5
Romário Lima <me@romar.io>
2016-02-23 01:42:57 -0300

    chore(karma): melhorias para CIs

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -50,6 +50,26 @@ const karmaBaseConfig = {
   }
 };
 
+const customLaunchers = {
+  chrome: {
+    base: 'SauceLabs',
+    browserName: 'chrome'
+  }
+};
+
+const ciAdditions = {
+  sauceLabs: {
+    testName: 'Karma Unit Tests',
+    startConnect: false,
+    build: process.env.TRAVIS_BUILD_NUMBER,
+    tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER
+  },
+  browsers: Object.keys(customLaunchers),
+  customLaunchers: customLaunchers,
+  reporters: ['progress', 'coverage', 'saucelabs']
+};
+
 module.exports = function (config) {
-  config.set(karmaBaseConfig);
+  const isCI = process.env.CI;
+  config.set(isCI ? Object.assign(karmaBaseConfig, ciAdditions) : karmaBaseConfig);
 };
