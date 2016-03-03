d99ee56fc432d148eb1e0aba2def0a5c92639ded
Romário Lima <me@romar.io>
2016-03-03 18:10:39 -0300

    wip (eu sei, fiz errado aqui)

--- app/js/states/definitions.js
+++ app/js/states/definitions.js
@@ -7,4 +7,13 @@ export default {
       description: 'Boierplate de apps Angular.js 1.x, Bootstrap 4, Browserify e Gulp 4',
     },
   },
+
+  answer: {
+    url: '/resposta',
+    templateUrl: 'answer.html',
+    data: {
+      title: 'A Resposta é ...',
+      description: 'Olha, talvez você fique um pouco chateado...',
+    },
+  },
 };
