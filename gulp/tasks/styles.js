d2a38c0fbf2c40714c073a56a22e3e297757d868
Romário Lima <me@romar.io>
2016-03-16 17:33:43 -0300

    chore(gulp/styles): adiciona test com o rev e rev-replace

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,9 +1,11 @@
 'use strict';
 
-import config from '../config.js'
+import config from '../config.js';
 import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import sass from 'gulp-sass';
+import rev from 'gulp-rev';
+import rename from 'gulp-rename';
 import sourcemaps from 'gulp-sourcemaps';
 import postcss from 'gulp-postcss';
 import gulpIf from 'gulp-if';
@@ -14,7 +16,7 @@ import bs from 'browser-sync';
 
 const postCssProcessors = [
   autoprefixer({ browsers: ['> 10%'] }),
-  mqpacker
+  mqpacker,
 ];
 
 export default styles;
@@ -26,10 +28,14 @@ function styles() {
   .pipe(sass({
     sourceComments: !global.isProd,
     outputStyle: global.isProd ? 'compressed' : 'nested',
-    includePaths: config.styles.sassIncludePaths
+    includePaths: config.styles.sassIncludePaths,
   }))
   .pipe(postcss(postCssProcessors))
   .pipe(gulpIf(!global.isProd, sourcemaps.write()))
+  .pipe(gulpIf(global.isProd, rev()))
   .pipe(gulp.dest(config.styles.dest))
+  .pipe(gulpIf(global.isProd, rename((path) => path.dirname = '/css/' )))
+  .pipe(gulpIf(global.isProd, rev.manifest({ merge: true })))
+  .pipe(gulpIf(global.isProd, gulp.dest('.')))
   .pipe(bs.stream());
 }
